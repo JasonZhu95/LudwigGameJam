48f3bceaa847e3a3310cde34f068f6acd67edc79
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2021-10-19 21:30:26 -0700

    Added Sound Manager Script

---
+++ Assets/Scripts/Manager/SoundManager/SoundManagerScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public static AudioClip playerDeath;
+    private static AudioSource audioSrc;
+
+    private void Start()
+    {
+        playerDeath = Resources.Load<AudioClip>("playerDeath");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "playerDeath":
+                audioSrc.PlayOneShot(playerDeath);
+                break;
+        }
+    }
+}
